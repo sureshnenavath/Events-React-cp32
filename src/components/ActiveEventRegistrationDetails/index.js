86549a88ad1a648982bbcff7ad612168203db3a3
Nenavath Suresh <133812638+sureshnenavath@users.noreply.github.com>
2025-03-16 15:08:17 +0530

    index.js

--- src/components/ActiveEventRegistrationDetails/index.js
+++ src/components/ActiveEventRegistrationDetails/index.js
@@ -8,7 +8,7 @@ const ActiveEventRegistrationDetails = props => {
       case 'YET_TO_REGISTER':
         return (
           <>
-            <img src={imageUrl} alt={altText} className="registration-image" />
+            <img src="https://assets.ccbp.in/frontend/react-js/events-register-img.png" alt="yet to register" className="registration-image" />
             <p className="registration-text">
               A live performance brings so much to your relationship with dance.
               Seeing dance live can often make you fall totally in love with
@@ -22,7 +22,7 @@ const ActiveEventRegistrationDetails = props => {
       case 'REGISTERED':
         return (
           <>
-            <img src={imageUrl} alt={altText} className="registration-image" />
+            <img src="https://assets.ccbp.in/frontend/react-js/events-regestered-img.png" alt="registered" className="registration-image" />
             <h1 className="registered-heading">
               You have already registered for the event
             </h1>
@@ -31,7 +31,7 @@ const ActiveEventRegistrationDetails = props => {
       case 'REGISTRATIONS_CLOSED':
         return (
           <>
-            <img src={imageUrl} alt={altText} className="registration-image" />
+            <img src="https://assets.ccbp.in/frontend/react-js/events-registrations-closed-img.png" alt="registrations closed" className="registration-image" />
             <h1 className="closed-heading">Registrations Are Closed Now!</h1>
             <p className="registration-text">
               Stay tuned. We will reopen the registrations soon!
